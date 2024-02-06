0acb559018e23ffa3b60a5d3fea826112b6d8afa
mahadi-init <init.mahadi@gmail.com>
2024-02-06 22:21:41 +0600

    data fetching improvement

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,40 +1,29 @@
 import PageTop from "@/components/native/PageTop";
-import getData from "@/actions/get";
-import { BACKEND_URL } from "@/consts/site-info";
 import StatisticCards from "@/components/native/StatisticCards";
 import SalesStatistics from "@/components/native/SalesStaistic";
 import MostSellingCategory from "@/components/native/MostSellingCategory";
-
-interface DashboardData {
-  todayOrderAmount: number;
-  yesterdayOrderAmount: number;
-  monthlyOrderAmount: number;
-  totalOrderAmount: number;
-}
+import {
+  getDashboardOrderData,
+  getMostSellingCategory,
+  getSalesOrdersStatistics,
+} from "@/utils/dashboard-data";
 
 export default async function Dashboard() {
+  const data = await Promise.all([
+    await getDashboardOrderData(),
+    await getSalesOrdersStatistics(),
+    await getMostSellingCategory(),
+  ]);
+
   const {
     todayOrderAmount,
     yesterdayOrderAmount,
     monthlyOrderAmount,
     totalOrderAmount,
-  } = await getData<DashboardData>(
-    `${BACKEND_URL}/api/user-order/dashboard-amount`,
-    300,
-    ["dashboard"],
-  );
+  } = data[0];
 
-  const mostsellingCategory = await getData(
-    `${BACKEND_URL}/api/user-order/most-selling-category`,
-    3600,
-  );
-
-  const report = await getData(
-    `${BACKEND_URL}/api/user-order/sales-report`,
-    3600,
-  );
-
-  console.log(mostsellingCategory);
+  const report = data[1];
+  const mostSelling = data[2];
 
   return (
     <div className="p-4 mt-12 lg:mt-4 lg:ml-72">
@@ -46,10 +35,8 @@ export default async function Dashboard() {
         totalOrderAmount={totalOrderAmount}
       />
       <div className="grid grid-cols-1 gap-4 items-center xl:grid-cols-2">
-        {/*@ts-expect-error*/}
-        <SalesStatistics data={report.salesReport} />
-        {/*@ts-expect-error*/}
-        <MostSellingCategory data={mostsellingCategory.categoryData} />
+        <SalesStatistics data={report} />
+        <MostSellingCategory data={mostSelling} />
       </div>
     </div>
   );
