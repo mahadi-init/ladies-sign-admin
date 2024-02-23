29359fa59a1059a170f59aff8c04b7c0b9e62217
mahadi-init <init.mahadi@gmail.com>
2024-02-23 17:14:50 +0600

    update brand route

--- src/app/dashboard/brand/edit/[id]/page.tsx
+++ src/app/dashboard/brand/edit/[id]/page.tsx
@@ -1,7 +1,9 @@
+import getData from "@/actions/get";
+import { patchData } from "@/actions/patch";
+import { Breadcrumbs } from "@/components/native/Breadcrumbs";
+import PageTop from "@/components/native/PageTop";
 import { BACKEND_URL } from "@/consts/site-info";
 import { BrandType } from "@/types/brand";
-import { patchData } from "@/actions/patch";
-import getData from "@/actions/get";
 import SharedBrandUI from "@/ui/SharedBrandUI";
 
 export default async function EditBrand({
@@ -11,16 +13,25 @@ export default async function EditBrand({
 }) {
   const data = await getData<BrandType>(
     `${BACKEND_URL}/api/brand/get/${params.id}`,
-    10,
+    0
   );
 
   return (
-    <SharedBrandUI
-      {...data}
-      queryUrl={`${BACKEND_URL}/api/brand/edit/${params.id}`}
-      validationTag="brands"
-      successMessage="Brand edited successfully"
-      serverAction={patchData}
-    />
+    <>
+      <PageTop title="Edit Brand" showSubTitle={false} />
+      <Breadcrumbs
+        props={[
+          { title: "Dashboard", link: "/dashboard" },
+          { title: "Brand", link: "/dashboard/brand" },
+        ]}
+      />
+      <SharedBrandUI
+        {...data}
+        queryUrl={`${BACKEND_URL}/api/brand/edit/${params.id}`}
+        validationTag="brands"
+        successMessage="Brand edited successfully"
+        serverAction={patchData}
+      />
+    </>
   );
 }
