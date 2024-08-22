40bdc177fbbd37a1e68f3c7f02dad561a7aa2a37
mahadi hasan <mahadi.dev@outlook.com>
2024-08-22 15:53:35 +0600

    refactor mongodb connnection

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,15 +1,14 @@
 import { site } from "@/site-config";
 import mongoose from "mongoose";
-//mongoose.set("strictQuery", true);
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(site.MONGO_URI as string);
-    if (connection.readyState === 1) {
-      return Promise.resolve(true);
+    const status = mongoose.connection.readyState;
+
+    if (status === 0) {
+      await mongoose.connect(site.MONGO_URI as string);
     }
   } catch (error) {
-    console.error(error);
     return Promise.reject(error);
   }
 };
